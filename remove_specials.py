65402b2e6f57ad63a7f25ed165be5e39339cd28e
ismailacar0 <ismaiilacar.3@gmail.com>
2024-06-30 15:50:36 +0300

    final version

--- remove_specials.py
+++
@@ -1,7 +0,0 @@
-import re
-
-
-def remove_specials(text):
-    text = text.lower().replace(' ', '_')
-    return re.sub("[^a-zA-Z0-9çÇğĞıİöÖşŞüÜ\$_]", '', text)
-print(remove_specials("çiRkin _kadın_"))
